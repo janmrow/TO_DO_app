8e1873d16a1bd7cc942a886f859cb9b70401b6bf
Jasiek <jasiek92@gmail.com>
2015-07-05 22:20:10 +0200

    editing task feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,25 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
   end
 
   def create
     Task.create(task_params)
     redirect_to :back
+  end
+
+  def edit
+    @task = Task.find params[:id]
+  end
+
+  def update
+    task = Task.find params[:id]
+    if task.update_attributes task_params
+      redirect_to tasks_path, notice: 'Task updated.'
+    else
+      redirect_to :back, notice: 'There was an error.'
+    end
   end
 
   private
--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
+
+  before do
+    @task = Task.create task: 'read a book' 
+  end
+
   describe "GET /tasks" do
       it "diplay some taks" do
-        @task = Task.create task: 'read a book' 
         visit tasks_path
         page.should have_content 'read a book'
       end
@@ -16,5 +20,32 @@ RSpec.describe "Tasks", type: :request do
         current_path == tasks_path
         page.should have_content 'walk with dog'
       end
+  end
+
+  describe "PUT /tasks" do 
+    it "edit a task" do
+      visit tasks_path
+      click_link 'Edit'
+
+      current_path.should == edit_task_path(@task)
+
+      expect(page).to have_selector("input[value='read a book']")  
+      fill_in 'Task', with: 'updated task'
+      click_button 'Update Task'
+
+      current_path.should == tasks_path
+      page.should have_content 'updated task'
+    end
+
+    it "should not update an empty task" do
+      visit tasks_path
+      click_link 'Edit'
+
+      fill_in 'Task', with: ''
+      click_button 'Update Task'
+
+      current_path.should == edit_task_path(@task)
+      expect(page).to have_content 'There was an error.'
+    end
   end
 end
