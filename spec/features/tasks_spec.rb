5b421ab6556cab0d9f1ba266ab3de96545d25ba0
Jasiek <jasiek92@gmail.com>
2015-07-05 15:18:17 +0200

    create tasks controller

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
       it "diplay some taks" do
         visit tasks_path
-        page.should have_content 'welcome'
+        page.should have_content 'read a book'
       end
   end
 end
