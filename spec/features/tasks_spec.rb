f11674167c1d96c2bbba19345be04c541697e16d
Jasiek <jasiek92@gmail.com>
2015-07-05 15:37:54 +0200

    displaying tasks

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
       it "diplay some taks" do
+        @task = Task.create task: 'read a book' 
         visit tasks_path
         page.should have_content 'read a book'
       end
